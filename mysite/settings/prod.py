0e267cbab315e92929addf3972257b1dc14900a2
Dmytro Serbeniuk <dserbeniuk@spscommerce.com>
2025-07-29 13:59:55 +0200

    add aws s3 prod

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -12,3 +12,33 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', 'eu-central-1')
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=2592000",
+}
+
+STATICFILES_LOCATION = "static"
+MEDIAFILES_LOCATION = "media"
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "mysite.storage_backends.MediaStorage"
+    },
+    "staticfiles": {
+        "BACKEND": "mysite.storage_backends.StaticStorage"
+    },
+}
