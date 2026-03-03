c8e1c64f8452cebef7c764184c54c772a44c3637
Dmytro Serbeniuk <dserbenyukgood@gmail.com>
2026-03-03 23:13:39 +0100

    translation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+set -e
+
 echo "Using settings: $DJANGO_SETTINGS_MODULE"
+
+echo "Updating translation catalogs..."
+python manage.py makemessages \
+  -l en -l uk \
+  -i "venv/*" -i ".venv/*" -i "env/*" \
+  -i "node_modules/*" -i "static/*" \
+  -i "*/site-packages/*" -i "build/*" -i "dist/*"
+
+echo "Compiling translation catalogs..."
+python manage.py compilemessages \
+  -i "venv/*" -i ".venv/*" -i "env/*" \
+  -i "node_modules/*" -i "static/*" \
+  -i "*/site-packages/*" -i "build/*" -i "dist/*"
 
 exec gunicorn -c gunicorn.conf.py mysite.wsgi:application
