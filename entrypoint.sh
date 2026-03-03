f480b4e8be5deb4b2ec345a464d73d1119057fd8
Dmytro Serbeniuk <dserbenyukgood@gmail.com>
2026-03-03 10:07:00 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ echo "Using settings: $DJANGO_SETTINGS_MODULE"
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 exec gunicorn -c gunicorn.conf.py mysite.wsgi:application
